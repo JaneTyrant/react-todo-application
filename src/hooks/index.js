7e513ef8d12015bf8bd79de9884e4ca05b4791ed
TyrantJane <nonstop00066@gmail.com>
2022-08-22 15:06:01 +0300

    Add useTodo hook

---
+++ src/hooks/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const useTodo = (initialTasks) => {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  return {
+    tasks,
+    addTask: (values) => {
+      console.log(values)
+      const newTask = {
+        id: Date.now(),
+        body: values.body,
+        isDone: false,
+      };
+      setTasks([...tasks, newTask])
+    },
+    deleteTask : (id) => {
+      setTasks(tasks.filter((task)=>task.id!==id))
+    },
+    setDoneTask: (id) => {
+        //map
+    }
+  };
+};
+
+export default useTodo;
