5bd271bd2dcec3f70bdc7d7e937bf94d2ee58541
TyrantJane <nonstop00066@gmail.com>
2022-08-23 23:40:44 +0300

    Refactor TaskInput component

--- src/components/Todo/TaskInput/index.js
+++ src/components/Todo/TaskInput/index.js
@@ -1,17 +1,35 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import cx from "classnames";
+import { SCHEMA_TASK } from "../../../utils/validateSchemas";
+import styles from "./TaskInput.module.scss";
 
 const TaskInput = (props) => {
-  const { addTask } = props;
+  const { addTask, name, ...restProps } = props;
   return (
-    <div>
-      <Formik initialValues={{ body: "" }} onSubmit={addTask}>
-        <Form>
-          <Field name="body" />
-          <input type="submit" value="+" />
-        </Form>
-      </Formik>
-    </div>
+    <Formik
+      initialValues={{ body: "" }}
+      onSubmit={addTask}
+      validationSchema={SCHEMA_TASK}
+    >
+      <Form>
+        <label className={styles.label}>
+          <Field name={name}>
+            {({ field, form, meta }) => {
+              const inputClasses = cx(styles.input, {
+                [styles.invalid]: meta.error && meta.touched,
+                [styles.valid]: !meta.error && meta.value,
+              });
+              return (
+                <input {...field} className={inputClasses} {...restProps} />
+              );
+            }}
+          </Field>
+          <ErrorMessage name={name} component="span" className={styles.error} />
+        </label>
+        <input type="submit" value="+" />
+      </Form>
+    </Formik>
   );
 };
 
