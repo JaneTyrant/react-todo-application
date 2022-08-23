2cc695ac85b8a35f6f3022b24f6338f2591cf2a5
TyrantJane <nonstop00066@gmail.com>
2022-08-23 23:39:37 +0300

    Refactor useTodo hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,23 +2,29 @@ import { useState } from "react";
 
 const useTodo = (initialTasks) => {
   const [tasks, setTasks] = useState(initialTasks);
-  
+  const addTask = (values, formikBag) => {
+    const newTask = {
+      id: Date.now(),
+      body: values.body,
+      isDone: false,
+    };
+    setTasks([...tasks, newTask]);
+    formikBag.resetForm();
+  };
+  const setDoneTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, isDone: !task.isDone } : { ...task }
+      )
+    );
+  };
   return {
     tasks,
-    addTask: (values) => {
-      const newTask = {
-        id: Date.now(),
-        body: values.body,
-        isDone: false,
-      };
-      setTasks([...tasks, newTask])
+    addTask,
+    setDoneTask,
+    deleteTask: (id) => {
+      setTasks(tasks.filter((task) => task.id !== id));
     },
-    deleteTask : (id) => {
-      setTasks(tasks.filter((task)=>task.id!==id))
-    },
-    setDoneTask: (id) => {
-        //map
-    }
   };
 };
 
