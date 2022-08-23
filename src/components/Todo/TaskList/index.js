47498b2f186ca9244b9c86286e5ef860889432ef
TyrantJane <nonstop00066@gmail.com>
2022-08-23 23:41:57 +0300

    Finalize TaskList

--- src/components/Todo/TaskList/index.js
+++ src/components/Todo/TaskList/index.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import SelectTask from "../SelectTask";
+import OptionValues from "../../../utils";
 
 const TaskList = (props) => {
-  const { tasks, deleteTask } = props;
+  const { tasks, deleteTask, setDoneTask } = props;
+  const [selectValue, setSelectValue] = useState(OptionValues.all);
+  const showTask = (task) => {
+    return (
+      <div key={`div${task.id}`}>
+        <label>
+          <input
+            type="checkbox"
+            value={task.isDone}
+            checked={task.isDone}
+            onChange={() => {
+              setDoneTask(task.id);
+            }}
+          />
+          <p key={task.id}>{task.body}</p>
+        </label>
+        <span
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+        >
+          X
+        </span>
+      </div>
+    );
+  };
+  const ShowSelected = () => {
+    if (selectValue === OptionValues.all) {
+      return tasks.map(showTask);
+    }
+    if (selectValue === OptionValues.done) {
+      return tasks.filter((task) => task.isDone).map(showTask);
+    }
+    if (selectValue === OptionValues.not_done) {
+      return tasks.filter((task) => !task.isDone).map(showTask);
+    }
+  };
   return (
-      <section>
-        {tasks.map((task) => (
-          <p key={task.id}>
-            {task.body}
-            <span
-              onClick={() => {
-                deleteTask(task.id);
-              }}
-            >
-              X
-            </span>
-          </p>
-        ))}
-      </section>
+    <>
+      <SelectTask selectValue={selectValue} setSelectValue={setSelectValue} />
+      <article>
+        <ShowSelected />
+      </article>
+    </>
   );
 };
 
