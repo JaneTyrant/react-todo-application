ad91dcd7819fa91a3e6724fc7d07ecf8d278dc89
TyrantJane <nonstop00066@gmail.com>
2022-08-27 00:25:03 +0300

    Refactor constants

--- src/utils/validateSchemas.js
+++ src/utils/validateSchemas.js
@@ -1,7 +1,8 @@
 import * as Yup from "yup";
 
 export const SCHEMA_TASK = Yup.object({
-  body: Yup.string("Must be string")
-    .matches(/^.{3,100}$/, "The task must contain from 3 to 100 symbols")
-    .required("Must be required"),
+  body: Yup.string()
+    .min(3, "Task must be at least 3 characters")
+    .max(100, "Task must be at most 100 characters")
+    .required("Enter your task here"),
 });
